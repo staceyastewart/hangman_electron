edfa1020d3e937a0146d09769b8ef506e0f86e15
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-05-05 15:30:19 -0400

    adds files

---
+++ main.js
@@ -0,0 +1,11 @@
+const electron = require(“electron”);
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+var mainWindow = null;
+app.on(“ready”, function(){
+  mainWindow = new BrowserWindow({
+    width:612,
+    height: 384
+    });
+  mainWindow.loadURL(`file://${__firname}/app/index.html`);
+});
