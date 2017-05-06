7fd88124da0b375c7540e80e5deeaa44d0078cfe
Laura Amayagit config --global user.email amaya.lauraa@gmail.com <amaya.lauraa@gmail.com>
2017-05-06 15:15:11 -0400

    frame

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 var mainWindow = null;
 
+
 app.on("ready", function(){
   mainWindow = new BrowserWindow({
     width:700,
-    height: 700
+    height: 700,
+    // frame:false,
+    titleBarStyle: 'hidden-inset'
     });
   // mainWindow.webContents.openDevTools();
   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
