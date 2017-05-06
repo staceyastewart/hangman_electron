163614559c0a545ee10a80fcf947bc1528c1d8a8
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-05-06 13:05:08 -0400

    finishes game logic

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 $(document).ready(function() {
-
-let body = $("body")
 let hangman = $(".hangman")
-let wins=0;
-let losses=0;
-
 let rightGuessCounter = 0;
 let wrongGuessCounter = 0;
-let arrayOfPhrases = ["cat", "dog", "armadillo", "chinchilla", "fine scott you win"]
+let arrayOfPhrases = ["cat", "dog", "armadillo", "chinchilla", "Matz is nice so we are nice", "Ruby on Rails", "Ottawa Canada", "Beijing China", "Jazz", "Rhythm", "JavaScript", "Stacey", "Laura", "Scott"]
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-let randomPhrase = arrayOfPhrases[getRandomInt(0,arrayOfPhrases.length)]
+let randomPhrase = arrayOfPhrases[getRandomInt(0,arrayOfPhrases.length)].toUpperCase()
 
 let wordOnPage = $(".randomWord");
 let appendLetters = function(str) {
@@ -22,26 +17,23 @@ let appendLetters = function(str) {
     if (str[i] === " "){
       wordOnPage.append("<div class='spaceOnPage'>  </div>");
     } else {
-    wordOnPage.append("<div class='letterOnPage' id='"+ i +"'>" + str[i]+ "</div>");
+    wordOnPage.append("<div class='letterOnPage' id='"+ i +"'>" + str[i] + "</div>");
     }
   }
 }
 appendLetters(randomPhrase);
 
-//below finds unique characters
-//found on stack overflow
+//below finds unique characters --- found on stack overflow
 var uniqueLetters = randomPhrase.split('').filter(function(item, i, ar){ return ar.indexOf(item) === i; }).join('').split(" ").join("");
 
-// let maxNumberOfRightGuesses = uniqueLetters.length
-
 let button = document.querySelectorAll("button")
+
 for (var i = 0; i < button.length; i++) {
   button[i].addEventListener("click", function(e){
   e.preventDefault()
   if(this.className != "clicked"){
-    guessedLetter = this.innerHTML.toLowerCase();
+    guessedLetter = this.innerHTML;
     this.setAttribute("class", "clicked");
-
     if(uniqueLetters.includes(guessedLetter)){
       rightGuessCounter++;
       for (var i = 0; i < randomPhrase.length; i++) {
@@ -51,41 +43,27 @@ for (var i = 0; i < button.length; i++) {
         }
       }
       if(rightGuessCounter === uniqueLetters.length){
-        wins++;
-        console.log(wins)
-        reset();
+        $(".gameOutcome").text("YOU WON!")
+        for (var i = 0; i < randomPhrase.length; i++) {
+          randomPhrase[i]
+        }
+        for (var i = 0; i < button.length; i++) {
+          button[i].setAttribute("class", "clicked")
+        }
       }
     } else{
       wrongGuessCounter++;
       hangman.attr('class', `hangman${wrongGuessCounter}`);
       if (wrongGuessCounter === 6){
-        console.log("YOU LOSE SIR!")
-        losses++;
-        console.log(losses)
-        reset()
+        $(".gameOutcome").text("YOU LOST!")
+        for (var i = 0; i < button.length; i++) {
+          button[i].setAttribute("class", "clicked")
+        }
       }
     }
   }
 })
 }
 
-let reset = function(){
-  console.log("TESTER")
-  rightGuessCounter=0;
-  wrongGuessCounter=0;
-  hangman.attr('class', 'hangman');
-  clearLetters();
-  let randomPhrase = arrayOfPhrases[getRandomInt(0,arrayOfPhrases.length)];
-  appendLetters(randomPhrase);
-}
-
-let clearLetters = function(){
-  let clickedButtons = $("#clicked");
-  clickedButtons.removeClass().attr("class","button");
-  for (var i = 0; i < randomPhrase.length; i++) {
-      let id = $(`#${i}`);
-      id.remove();
-    }
-}
 
 }); //JQUERY closure
